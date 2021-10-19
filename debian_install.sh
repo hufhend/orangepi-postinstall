7f2a8e7cc54f7c0603a431dd83f0995a59adf670
Václav Dvorský <vaclav.dvorsky@inventi.cz>
2021-10-19 22:44:20 +0200

    Add option for Ubuntu focal

--- debian_install.sh
+++ debian_install.sh
@@ -25,7 +25,10 @@ if ! [ $(id -u) = 0 ]; then
     # docker installation
     sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
     curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    # for Debian
     echo "deb [arch=arm64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    # for Ubuntu
+    # echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update
     apt-cache policy docker-ce
     sudo apt install -y docker-ce docker-ce-cli containerd.io
